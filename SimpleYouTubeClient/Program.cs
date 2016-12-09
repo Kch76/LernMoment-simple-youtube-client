0d79577f812cfd957c17af5fa68b0f44e58bb94a
Jan Suchotzki <jan@lernmoment.de>
2016-12-09 13:44:50 +0100

    Fügt initiale Proxy-Klasse hinzu und verwendet sie

--- SimpleYouTubeClient/Program.cs
+++ SimpleYouTubeClient/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Google.Apis.YouTube.v3.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,20 @@ namespace SimpleYouTubeClient
     {
         static void Main(string[] args)
         {
+            YouTubeServerProxy proxy;
+            Video[] uploadedVideos;
+
+            Console.WriteLine("### Willkommen zum SYT-Client! ###");
+            Console.WriteLine();
+
+            Console.Write("Bitte gebe deinen API-Key ein: ");
+            string apiKey = Console.ReadLine();
+            proxy = new YouTubeServerProxy(apiKey);
+
+            Console.Write("Bitte gebe eine Channel-Id ein: ");
+            string channelId = Console.ReadLine();
+            uploadedVideos = proxy.GetAllUploadedVideos(channelId);
+            Console.ReadLine();
         }
     }
 }
---
+++ SimpleYouTubeClient/YouTubeServerProxy.cs
@@ -0,0 +1,46 @@
+﻿using Google.Apis.Services;
+using Google.Apis.YouTube.v3;
+using Google.Apis.YouTube.v3.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleYouTubeClient
+{
+    class YouTubeServerProxy
+    {
+        private YouTubeService ytService;
+
+        public YouTubeServerProxy(string apiKey)
+        {
+            ytService = new YouTubeService(new BaseClientService.Initializer() { ApiKey = apiKey });
+        }
+
+        public Video[] GetAllUploadedVideos(string channelId)
+        {
+            // Get ids of all uploaded videos from the channel
+            string[] videoIds = GetIdsOfAllUploadedVideos(channelId);
+
+            // Get core information of each video specified by id
+            Video[] allUploadedVideos = GetVideoDataForAllIds(videoIds);
+
+            return allUploadedVideos;
+        }
+
+        private Video[] GetVideoDataForAllIds(string[] videoIds)
+        {
+            Console.WriteLine("GetVideoDataForAllIds to be implemented!");
+
+            return null;
+        }
+
+        private string[] GetIdsOfAllUploadedVideos(string channelId)
+        {
+            Console.WriteLine("GetIdsOfAllUploadedVideos to be implemented!");
+
+            return null;
+        }
+    }
+}
